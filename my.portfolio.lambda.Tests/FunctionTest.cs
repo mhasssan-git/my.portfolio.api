e912d91af4ebd803cb769c17d55840536507965d
Mahmudul Hassan <93487707+MahmudHassanLalin@users.noreply.github.com>
2024-01-25 16:31:51 -0600

    smtp-setup

--- my.portfolio.lambda.Tests/FunctionTest.cs
+++ my.portfolio.lambda.Tests/FunctionTest.cs
@@ -1,20 +1,24 @@
 using Xunit;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
+using Amazon.SimpleEmail.Model;
 
 namespace my.portfolio.lambda.Tests;
 
 public class FunctionTest
 {
     [Fact]
-    public void TestToUpperFunction()
+    public async  void TestToUpperFunction()
     {
-
+        var input = "{\"From\": \"lalinmail@gmail.com\"," +
+                    "\"To\": \"lalinmail@gmail.com\"," +
+                    "\"Subject\": \"Test Data From Lambda\"," +
+                    "\"Body\": \"Hi How Are You\"}";
         // Invoke the lambda function and confirm the string was upper cased.
         var function = new Function();
         var context = new TestLambdaContext();
-        var upperCase = function.FunctionHandler("hello world", context);
+        var output = await function.FunctionHandler(input, context);
+        Assert.Contains("ok", output.ToLower());
 
-        Assert.Equal("HELLO WORLD", upperCase);
     }
 }
--- my.portfolio.lambda/Function.cs
+++ my.portfolio.lambda/Function.cs
@@ -1,21 +1,90 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Amazon.Lambda.Core;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace my.portfolio.lambda;
 
+
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public async Task<string> FunctionHandler(string input, ILambdaContext context)
     {
-        return input.ToUpper();
+        var mailRequest = JsonSerializer.Deserialize<MailRequest>(input);
+        var email = new MimeMessage { Sender = MailboxAddress.Parse(mailRequest?.From) };
+        email.From.Add(MailboxAddress.Parse(mailRequest?.From));
+        email.To.Add(MailboxAddress.Parse(mailRequest?.To));
+        email.Subject=mailRequest?.Subject;
+        email.Body= new EmailBodyBuilder()
+            .BuildEmailBody(mailRequest?.Body)
+            .Build();
+
+        using var smtp = new SmtpClient();
+        
+        await smtp.ConnectAsync(MailSettings.Host, MailSettings.Port,
+            SecureSocketOptions.StartTls);
+        await smtp.AuthenticateAsync(MailSettings.UserName, MailSettings.Password);
+        return  await smtp.SendAsync(email);
     }
+}
+
+public static class MailSettings
+{
+    public static string EmailFrom => "xef786@gmail.com";
+    public static string UserName => "xef786@gmail.com";
+    public static string Password => "gxfqfmqeyenenodv";
+    public static string Host => "smtp.gmail.com";
+    public static int Port => 587;
+}
+
+public class EmailBodyBuilder
+{
+    private BodyBuilder builder;
+
+    public EmailBodyBuilder()
+    {
+        builder = new BodyBuilder();
+    }
+
+    public EmailBodyBuilder BuildEmailBody(string body)
+    {
+        builder.HtmlBody = body;
+        return this;
+    }
+
+    public MimeEntity Build()
+    {
+        return builder.ToMessageBody();
+    }
+}
+
+public class MailRequest
+{
+    public MailRequest(string from, string to, string subject, string body)
+    {
+        From = from;
+        To = to;
+        Subject = subject;
+        Body = body;
+    }
+
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
 }
